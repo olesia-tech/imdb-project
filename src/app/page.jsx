6effad4bad5c585b563822bf7b386c1f13e9590a
olesia-tech <shikotanshop@gmail.com>
2023-08-30 15:19:07 -0400

    added error handling for async function Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY
 
+//server-side rendering
 export default async function Home({searchParams}) {
 const genre = searchParams.genre || 'fetchTrending';
 const res = await fetch( `https://api.themoviedb.org/3/${
